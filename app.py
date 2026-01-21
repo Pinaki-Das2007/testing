dfb23d7a4f6f09a011f0895d69ecb0cf4c9fe7db
pinaki007 <daspinaki151@gmail.com>
2026-01-21 18:47:26 +0530

    changes on 21st

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_template, request,redirect,flash,url_for
+from flask import Flask, render_template, request, redirect, flash, url_for
 
 app = Flask(__name__)
 app.secret_key = "secret_key"
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
-    if request.form:
-        name = request.form['username']
+    if request.method == "POST":
+        name = request.form.get('username')
+
         if not name:
             flash("Please enter your name")
             return redirect(url_for('form'))
-        flash("Thank you for your feedback, " + name + "!")
-        return render_template("thankyou")
+
+        flash(f"Thank you for your feedback, {name}!")
+        return redirect(url_for('thankyou'))
+
     return render_template("form.html")
 
 @app.route('/thankyou')
 def thankyou():
     return render_template("thankyou.html")
-    
